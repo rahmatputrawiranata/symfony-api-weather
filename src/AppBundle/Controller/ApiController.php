15c90fb249a766db42902c8f3b0d86ea8d8c2082
rahmatputrawiranata <webdev.blackards@gmail.com>
2018-12-14 10:09:18 +0700

    test api with jquery

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -7,17 +7,25 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ApiController extends Controller
 {
     /**
      * @Route("/api/location", name="locationList")
+     * @Method("GET")
      */
     public function locationList(Request $request)
     {   
-        $request->isXmlHttpRequest();
-        $request->query->get('page');
-        return $this->json(array('username' => 'jane.doe'));
 
+        $jPath = file_get_contents(__DIR__.'\dummy\au_city.json');
+        $data = json_decode($jPath, true);
+        return new jsonResponse(
+            [
+                'msg'=>'data',
+                'data'=> $data
+            ],
+            Response::HTTP_OK
+        );
     }
 }
