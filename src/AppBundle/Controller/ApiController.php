35b1c27a5936643427298b4f61d013f1334d4ca6
rahmatputrawiranata <webdev.blackards@gmail.com>
2018-12-15 16:29:59 +0700

    add api

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -37,6 +37,8 @@ class ApiController extends Controller
      */
     public function weatherMap(Request $request)
     {
+
+        $api_key = '0c0a594e554a11447c34e2533ae5c803';
         $city = $request->request->get('where');
         $city = explode(" - ", $city);
         $cityF = $city[1];
@@ -44,10 +46,22 @@ class ApiController extends Controller
         $json = json_decode($jPath, true);
         foreach($json as $k){
             if($k['Place Name'] == $cityF){
-                return new jsonResponse(
-                    $k,
-                    Response::HTTP_OK
-                );
+                    $lat = $k['Latitude'];
+                    $lon = $k['Longitude'];
+                
+                    $api_key = '0c0a594e554a11447c34e2533ae5c803';
+                    $weather_map = 'http://api.openweathermap.org/data/2.5/weather?lat='.$lat.'&lon='.$lon.'&appid='.$api_key;
+                    $ch = curl_init();
+                    curl_setopt($ch,CURLOPT_URL, $weather_map);
+                    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+                    $output=curl_exec($ch);
+                    curl_close($ch);
+
+                    $data = json_decode($output);
+                    return new jsonResponse(
+                        $data,
+                        Response::HTTP_OK
+                    );
             }
         }
     }
